3f763df97e99eed64a057af0ac0af2c85fd763d3
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-08 12:05:47 -0700

    Create DriveCircle.java

---
+++ DriveCircle.java
@@ -0,0 +1,50 @@
+// simple autonomous program that drives bot in a circle then ends.
+// this code assumes it will end before the period is over but if the period ended while
+// still driving, this code will just stop.
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="Drive Circle", group="Exercises")
+//@Disabled
+public class DriveCircle extends LinearOpMode
+{
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+
+    // called when init button is  pressed.
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        leftMotor = hardwareMap.dcMotor.get("left_motor");
+        rightMotor = hardwareMap.dcMotor.get("right_motor");
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Mode", "waiting");
+        telemetry.update();
+
+        // wait for start button.
+
+        waitForStart();
+
+        telemetry.addData("Mode", "running");
+        telemetry.update();
+
+        sleep(500);              // wait so that above telemetry is visible.
+
+        // set power levels 75% left and 10% right to drive in an arc to the right.
+
+        leftMotor.setPower(-0.75);
+        rightMotor.setPower(-0.10);
+
+        sleep(5000);            // drive 5 seconds to make a circle.
+
+        // turn the motors off.
+
+        rightMotor.setPower(0);
+        leftMotor.setPower(0);
+    }
+}
