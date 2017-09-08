b6175e7ad3b49c9122cf16686fe60404c3c79c6c
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-08 12:27:20 -0700

    Create DriveWithGripper.java

---
+++ DriveWithGripper.java
@@ -0,0 +1,91 @@
+// simple teleop program that drives bot using controller joysticks in tank mode.
+// this code monitors the period and stops when the period is ended. Also uses
+// controller button A to lower the arm, button B to raise the arm. Controller
+// button X opens the gripper and button Y closes the gripper.
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Drive Gripper", group="Exercises")
+//@Disabled
+public class DriveWithGripper extends LinearOpMode
+{
+    DcMotor leftMotor, rightMotor;
+    Servo   armServo, gripServo;
+    float   leftY, rightY;
+    double  armPosition, gripPosition;
+
+    // called when init button is  pressed.
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        leftMotor = hardwareMap.dcMotor.get("left_motor");
+        rightMotor = hardwareMap.dcMotor.get("right_motor");
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        armServo = hardwareMap.servo.get("arm_servo");
+        gripServo = hardwareMap.servo.get("grip_servo");
+
+        telemetry.addData("Mode", "waiting");
+        telemetry.update();
+
+        // wait for start button.
+
+        waitForStart();
+
+        armPosition = .5;                    // set arm to half way up.
+        gripPosition = Servo.MAX_POSITION;   // set grip to full open.
+
+        while (opModeIsActive())
+        {
+            leftY = gamepad1.left_stick_y;
+            rightY = gamepad1.right_stick_y;
+
+            leftMotor.setPower(Range.clip(leftY, -1.0, 1.0));
+            rightMotor.setPower(Range.clip(rightY, -1.0, 1.0));
+
+            telemetry.addData("Mode", "running");
+            telemetry.addData("sticks", "  left=" + leftY + "  right=" + rightY);
+
+            // check the gamepad buttons and if pressed, increment the appropriate position
+            // variable to change the servo location.
+
+            // move arm down on A button if not already at lowest position.
+            if (gamepad1.a && armPosition > Servo.MIN_POSITION) armPosition -= .01;
+
+            // move arm up on B button if not already at the highest position.
+            if (gamepad1.b && armPosition < Servo.MAX_POSITION) armPosition += .01;
+
+            // open the gripper on X button if not already at most open position.
+            if (gamepad1.x && gripPosition < Servo.MAX_POSITION) gripPosition = gripPosition + .01;
+
+            // close the gripper on Y button if not already at the closed position.
+            if (gamepad1.y && gripPosition > Servo.MIN_POSITION) gripPosition = gripPosition - .01;
+
+            // set the servo position values as we have computed them.
+            armServo.setPosition(Range.clip(armPosition, Servo.MIN_POSITION, Servo.MAX_POSITION));
+            gripServo.setPosition(Range.clip(gripPosition, Servo.MIN_POSITION, Servo.MAX_POSITION));
+
+            telemetry.addData("arm servo", "position=" + armPosition + "  actual=" + armServo.getPosition());
+            telemetry.addData("grip servo", "position=" + gripPosition + "    actual=" + gripServo.getPosition());
+
+            //telemetry.addData("arm servo", String.format("position=%.2f  actual=%.2f", armPosition,
+            //    armServo.getPosition()));
+
+            //telemetry.addData("grip servo", String.format("position=%.2f  actual=%.2f", gripPosition,
+            //    gripServo.getPosition()));
+            //telemetry.addData("arm servo", "position=%.2f  actual=%.2f", armPosition,
+            //    armServo.getPosition());
+
+            //telemetry.addData("grip servo", "position=%.2f  actual=%.2f", gripPosition, gripServo.getPosition());
+
+            telemetry.update();
+            idle(); 
+        }
+    }
+}
