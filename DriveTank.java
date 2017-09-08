0889d928c5ea6a14ca24d721bdc15f10f7048d5d
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-08 12:08:07 -0700

    Create DriveTank.java

---
+++ DriveTank.java
@@ -0,0 +1,48 @@
+// simple teleop program that drives bot using controller joysticks in tank mode.
+// this code monitors the period and stops when the period is ended.
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Drive Tank", group="Exercises")
+//@Disabled
+public class DriveTank extends LinearOpMode
+{
+    DcMotor leftMotor, rightMotor;
+    float   leftY, rightY;
+
+    // called when init button is  pressed.
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        leftMotor = hardwareMap.dcMotor.get("left_motor");
+        rightMotor = hardwareMap.dcMotor.get("right_motor");
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Mode", "waiting");
+        telemetry.update();
+
+        // wait for start button.
+
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            leftY = gamepad1.left_stick_y;
+            rightY = gamepad1.right_stick_y;
+
+            leftMotor.setPower(Range.clip(leftY, -1.0, 1.0));
+            rightMotor.setPower(Range.clip(rightY, -1.0, 1.0));
+
+            telemetry.addData("Mode", "running");
+            telemetry.addData("sticks", "  left=" + leftY + "  right=" + rightY);
+            telemetry.update();
+
+            idle();
+        }
+    }
+}
