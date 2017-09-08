e06f36722b4f0f6bed9001b53daff80450fcc016
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-08 11:58:28 -0700

    Delete HardwareMapping.java

--- HardwareMapping.java
+++
@@ -1,7 +0,0 @@
-DCMotor leftMotor, rightMotor;
-
-leftMotor = hardwareMap.dcMotor.get("left_motor");
-rightMotor = hardwareMap.dcMotor.get("right_motor");
-
-leftMotor.setPower(1.0);
-rightMotor.setPower(1.0);
