1e87eb52d96d21d0005484df4a28f220fb42cdc9
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-10-09 10:54:14 -0700

    Update MyOpMode.java

--- MyOpMode.java
+++ MyOpMode.java
@@ -5,8 +5,7 @@ package org.firstinspires.ftc.teamcode;
 // Now we import the classes we need from the FTC SDK and the Java SDK.
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,51 +16,28 @@ import java.util.Date;
 // menu of OpModes you can run.
 
 @Autonomous(name="MyOpMode")
-public class MyOpMode extends OpMode 
+public class MyOpMode extends LinearOpMode 
 {
   private String startDate;
-  private ElapsedTime runtime = new ElapsedTime();
+  
+  // Create a string containing the current date and time.
+  
+  startDate = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());
 
-  // Here is the init() method. We don't have anything to do here so we could
-  // have left it out.
-
-  @Override
-  public void init() 
+  // Pause program, wait for start button press.
+  
+  waitForStart();
+  
+  // Start button pressed, off we go.
+  
+  runtime.reset();
+  
+  // Now loop displaying the updated run time of this opMode until stop button is pressed.
+  
+  while (opModeIsActive())
   {
-  }
-
-  // Here we are intializing the variables we are using each time we run the
-  // OpMode.
-
-  @Override
-  public void init_loop() 
-  {
-    startDate = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());
-    runtime.reset();
-    telemetry.addData("MyOpMode Init Loop", runtime.toString());
-  }
-
-  // Here are the start() and stop() methods. We don't have anything to do at those
-  // times so we could have left them out.
-
-  @Override
-  public void start()
-  {
-  }
-
-  @Override
-  public void stop()
-  {
-  }
-
-  // The loop() method is called over and over until the Stop button is pressed.
-  // The method displays the elapsed run time on the driver stattion using the
-  // telemtry field of the base OpMode class.
-
-  @Override
-  public void loop() 
-  {
-    telemetry.addData("1 Start", "MyOpMode started at " + startDate);
-    telemetry.addData("2 Status", "running for " + runtime.toString());
+    telemetry.addData("Start", "MyOpMode started at " + startDate);
+    telemetry.addData("Status", "running for " + runtime.toString());
+    idle();
   }
 }
