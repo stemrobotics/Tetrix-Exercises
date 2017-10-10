1080f72180a196b04f3e3e79b244b39dfedfeca4
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-10-10 11:06:22 -0700

    Create TimingDemo.java

---
+++ TimingDemo.java
@@ -0,0 +1,40 @@
+// simple teleop program that demonstrates timing issues.
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@TeleOp(name="Timing Demo", group="Exercises")
+//@Disabled
+public class TimingDemo extends LinearOpMode
+{
+    int     buttonCount;
+    boolean bButton;
+
+    // called when init button is  pressed.
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        telemetry.addData("Mode", "waiting");
+        telemetry.update();
+
+        // wait for start button.
+
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            if (gamepad1.a) buttonCount += 1;
+
+            if (gamepad1.b) bButton = !bButton;
+
+            telemetry.addData("Mode", "running");
+            telemetry.addData("A button Count", buttonCount);
+            telemetry.addData("B button", bButton);
+            telemetry.update();
+
+            idle();
+        }
+    }
+}
