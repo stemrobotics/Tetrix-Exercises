d819bdd5ceec47a02d1dbb4a254f4de1f461fa70
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2019-02-04 17:36:25 -0800

    Update DriveAvoidImu.java

--- DriveAvoidImu.java
+++ DriveAvoidImu.java
@@ -12,7 +12,7 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
