e4e5b3fec34cde2d479afb3abe4dfea4ef60f9b6
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2019-04-11 15:15:38 -0700

    Update DriveAvoidPid.java

--- DriveAvoidPid.java
+++ DriveAvoidPid.java
@@ -268,8 +268,8 @@ public class DriveAvoidPid extends LinearOpMode
         private boolean m_continuous = false;	// do the endpoints wrap around? eg. Absolute encoder
         private boolean m_enabled = false;      // is the pid controller enabled
         private double m_prevError = 0.0;       // the prior sensor input (used to compute velocity)
-        private double m_totalError = 0.0;      //the sum of the errors for use in the integral calc
-        private double m_tolerance = 0.05;      //the percentage error that is considered on target
+        private double m_totalError = 0.0;      // the sum of the errors for use in the integral calc
+        private double m_tolerance = 0.05;      // the percentage error that is considered on target
         private double m_setpoint = 0.0;
         private double m_error = 0.0;
         private double m_result = 0.0;
