28d31a3d081eb2c28a2aa11551d81a91e8689088
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2020-06-20 17:10:58 -0700

    Update DriveWithEncoder.java

--- DriveWithEncoder.java
+++ DriveWithEncoder.java
@@ -1,13 +1,11 @@
 // autonomous program that drives bot forward a set distance, stops then
 // backs up to the starting point using encoders to measure the distance.
-// This example assumes there is one encoder, attached to the left motor.
 
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorController;
 
 @Autonomous(name="Drive Encoder", group="Exercises")
 //@Disabled
@@ -21,17 +19,23 @@ public class DriveWithEncoder extends LinearOpMode
     {
         leftMotor = hardwareMap.dcMotor.get("left_motor");
         rightMotor = hardwareMap.dcMotor.get("right_motor");
-        
+
+        // You will need to set this based on your robot's
+        // gearing to get forward control input to result in
+        // forward motion.
         leftMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        // reset encoder count kept by left motor.
+        // reset encoder counts kept by motors.
         leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        // set left motor to run to target encoder position and stop with brakes on.
+        // set motors to run forward for 5000 encoder counts.
+        leftMotor.setTargetPosition(5000);
+        rightMotor.setTargetPosition(5000);
+
+        // set motors to run to target encoder position and stop with brakes on.
         leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        // set right motor to run without regard to an encoder.
-        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         telemetry.addData("Mode", "waiting");
         telemetry.update();
@@ -43,20 +47,19 @@ public class DriveWithEncoder extends LinearOpMode
         telemetry.addData("Mode", "running");
         telemetry.update();
 
-        // set left motor to run to 5000 encoder counts.
-
-        leftMotor.setTargetPosition(5000);
-        
-        // set both motors to 25% power. Movement will start.
+        // set both motors to 25% power. Movement will start. Sign of power is
+        // ignored as sign of target encoder position controls direction when
+        // running to position.
 
         leftMotor.setPower(0.25);
         rightMotor.setPower(0.25);
 
         // wait while opmode is active and left motor is busy running to position.
 
-        while (opModeIsActive() && leftMotor.isBusy())   
+        while (opModeIsActive() && leftMotor.isBusy())   //leftMotor.getCurrentPosition() < leftMotor.getTargetPosition())
         {
-            telemetry.addData("encoder-fwd", leftMotor.getCurrentPosition() + "  busy=" + leftMotor.isBusy());
+            telemetry.addData("encoder-fwd-left", leftMotor.getCurrentPosition() + "  busy=" + leftMotor.isBusy());
+            telemetry.addData("encoder-fwd-right", rightMotor.getCurrentPosition() + "  busy=" + rightMotor.isBusy());
             telemetry.update();
             idle();
         }
@@ -67,28 +70,35 @@ public class DriveWithEncoder extends LinearOpMode
         leftMotor.setPower(0.0);
         rightMotor.setPower(0.0);
 
-        // wait 5 sec so you can observe the final encoder position.
+        // wait 5 sec to you can observe the final encoder position.
 
         resetStartTime();
 
         while (opModeIsActive() && getRuntime() < 5)
         {
-            telemetry.addData("encoder-fwd-end", leftMotor.getCurrentPosition() + "  busy=" + leftMotor.isBusy());
+            telemetry.addData("encoder-fwd-left-end", leftMotor.getCurrentPosition());
+            telemetry.addData("encoder-fwd-right-end", rightMotor.getCurrentPosition());
             telemetry.update();
             idle();
         }
 
-        // Now back up to starting point. In this example instead of
-        // having the motor monitor the encoder, we will monitor the encoder ourselves.
+        // From current position back up to starting point. In this example instead of
+        // having the motor monitor the encoder we will monitor the encoder ourselves.
 
         leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        leftMotor.setTargetPosition(0);
+        rightMotor.setTargetPosition(0);
+
+        // Power sign matters again as we are running without encoder.
         leftMotor.setPower(-0.25);
         rightMotor.setPower(-0.25);
 
-        while (opModeIsActive() && leftMotor.getCurrentPosition() > 0)
+        while (opModeIsActive() && leftMotor.getCurrentPosition() > leftMotor.getTargetPosition())
         {
-            telemetry.addData("encoder-back", leftMotor.getCurrentPosition());
+            telemetry.addData("encoder-back-left", leftMotor.getCurrentPosition());
+            telemetry.addData("encoder-back-right", rightMotor.getCurrentPosition());
             telemetry.update();
             idle();
         }
@@ -98,13 +108,12 @@ public class DriveWithEncoder extends LinearOpMode
         leftMotor.setPower(0.0);
         rightMotor.setPower(0.0);
 
-        // wait 5 sec so you can observe the final encoder position.
-
         resetStartTime();
 
         while (opModeIsActive() && getRuntime() < 5)
         {
-            telemetry.addData("encoder-back-end", leftMotor.getCurrentPosition());
+            telemetry.addData("encoder-back-left-end", leftMotor.getCurrentPosition());
+            telemetry.addData("encoder-back-right-end", rightMotor.getCurrentPosition());
             telemetry.update();
             idle();
         }
