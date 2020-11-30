643002c4edf0d0b3d6e5639a952dd8b076fef489
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2020-11-30 14:21:59 -0800

    Update VuforiaCapture.java

--- VuforiaCapture.java
+++ VuforiaCapture.java
@@ -2,26 +2,39 @@ package org.firstinspires.ftc.teamcode;
 
 import android.graphics.Bitmap;
 
+import com.qualcomm.robotcore.util.RobotLog;
+
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.TimeUnit;
 
 /**
- * This class provides a way to capture video frames (images) from the camera
- * when using Vuforia. Vuforia must own the camera so if we  want to do image
- * processing alongside Vuforia, we have to get the images from it. Once
+ * This class provides a way to capture video images from the camera
+ * when using Vuforia. This allows you use the images for other purposes
+ * like streaming to a browser or processing with OpenCV.
+ *
+ * Vuforia must own the camera so if we  want to do image
+ * processing alongside Vuforia, we have to get the images from Vuforia. Once
  * Vuforia is configured create an instance of this class with the localizer
  * object and then you can get images posted by Vuforia for external use.
  * Note that this means the images change on Vuforia's schedule and that can
  * be fairly slow. Even if you call for images rapidly, the image may not
- * change for 10s of milliseconds. This class works with web cams and phones.
+ * change for 10s of milliseconds. However testing shows adequate an refresh
+ * rate for useful video processing. This class uses a separate thread to
+ * monitor Vuforia for new images so the calling thread is not blocked
+ * waiting for a new image. This class works with web cams and phones.
  */
 class VuforiaCapture
 {
     private VuforiaLocalizer    vuforia;
 
     private BlockingQueue<VuforiaLocalizer.CloseableFrame> frameQueue;
+
+    private Bitmap          currentImage;
+
+    private Object          lock = new Object();
+
+    private MonitorVuforia monitorThread;
 
     /**
      * Constructor.
@@ -40,30 +53,54 @@ class VuforiaCapture
     {
         vuforia.enableConvertFrameToBitmap();
 
-        vuforia.setFrameQueueCapacity(10);
+        vuforia.setFrameQueueCapacity(1);
 
         frameQueue = vuforia.getFrameQueue();
+
+        monitorThread = new MonitorVuforia();
+
+        monitorThread.start();
     }
 
     /**
-     * Get a camera frame from Vuforia if one is available. May take up
-     * to 1 ms to return. Vuforia makes the frames available on it's own
-     * timing and it may be 10s of ms between new images. If an image is
-     * ready, this call returns immediately. If no image available, then
-     * this call will wait 1ms for a frame or return null. Internally we
-     * queue up to 10 frames but if you call this in the main opmode loop
-     * the queue will likely have none or 1 or 2 images. Queue only helps
-     * if you are taking frames slower than Vuforia is updating the queue.
-     * It this usually the other way around.
-     * @return Camera frame (image). Returns null if no image available.
+     * Get the current (last posted) image from Vuforia. This image is
+     * updated by vuforia on its own schedule typically in the 10s of
+     * milliseconds.
+     * @return Camera image. Null if no image available.
      */
-    public Bitmap getFrame()
+    public Bitmap getImage()
     {
-        if (frameQueue == null) return null;
+        synchronized (lock) { return currentImage; }
+    }
 
-        try
+    // Thread to monitor Vuforia for camera images passed through the blocking queue.
+
+    private class MonitorVuforia extends Thread
+    {
+        Bitmap  newImage;
+
+        public void run()
         {
-            return vuforia.convertFrameToBitmap(frameQueue.poll(1, TimeUnit.MILLISECONDS));
-        } catch (Exception e) { return null; }
+            try
+            {
+                while (true)
+                {
+                    // Note that take() waits until there is something in the queue to take.
+                    // This means this loop will iterate at the speed at which Vuforia updates
+                    // the queue. The queue length of 1 assumes this loop will empty the queue
+                    // as rapidly as Vuforia updates it. Vuforia returns frames which contain
+                    // more than one image format so we extract the bitmap image from the frame.
+                    // We copy the retrieved image so that is can be available to callers during
+                    // the time this thread waits for a new image.
+
+                    newImage = vuforia.convertFrameToBitmap(frameQueue.take());
+
+                    synchronized (lock)
+                    {
+                        currentImage = newImage.copy(newImage.getConfig(), false);
+                    }
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+        }
     }
 }
